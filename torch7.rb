7a2446ce2c312664c0ca67e847e49d9d4a00d8ea
Alex Burka <aburka@seas.upenn.edu>
2013-04-11 18:21:23 -0400

    new formula for Torch7

---
+++ torch7.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Torch7 < Formula
+  homepage 'http://www.torch.ch/'
+  head 'git://github.com/andresy/torch.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'homebrew/versions/gcc48' => :build
+  depends_on 'readline'
+  depends_on 'gnuplot'
+  depends_on 'qt4'
+  depends_on 'homebrew/science/openblas'
+
+  def install
+    ENV['CC'] = 'gcc-4.8'
+    ENV['CXX'] = 'g++-4.8'
+    mkdir 'build' do
+      system 'cmake', '..', *std_cmake_args
+      system 'make install'
+    end
+  end
+
+  test do
+    system 'torch -h'
+  end
+end
