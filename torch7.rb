79187c9add60e161af3e7522407ae0ca4e5d5c1b
Alex Burka <aburka@seas.upenn.edu>
2013-04-11 18:40:39 -0400

    add stable version

--- torch7.rb
+++ torch7.rb
@@ -3,6 +3,9 @@ require 'formula'
 class Torch7 < Formula
   homepage 'http://www.torch.ch/'
   head 'https://github.com/andresy/torch.git'
+  url 'https://github.com/andresy/torch/archive/7.0.3-0.zip'
+  version '7.0.3-0'
+  sha1 'd455003b3f632ffd5062f98de774479c1de57607'
 
   depends_on 'cmake' => :build
   depends_on 'homebrew/versions/gcc48' => :build
